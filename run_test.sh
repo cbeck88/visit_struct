d95d0f8488cfba5eae1a24f86aed81a6d02224d6
Chris Beck <beck.ct@gmail.com>
2016-06-23 00:40:31 -0400

    fixup travis: gcc 4.8 does not support C++14

--- run_test.sh
+++ run_test.sh
@@ -5,11 +5,15 @@ set -e
 $CXX $@ -std=c++11 -I. -Wall -Werror -Wextra -pedantic test_visit_struct.cpp
 ./a.out
 
-$CXX $@ -std=c++14 -I. -Wall -Werror -Wextra -pedantic test_visit_struct.cpp
-./a.out
-
 $CXX $@ -std=c++11 -I. -I/usr/include -Wall -Werror -Wextra -pedantic test_visit_struct_boost_fusion.cpp
 ./a.out
 
-$CXX $@ -std=c++14 -I. -I/usr/include -Wall -Werror -Wextra -pedantic test_visit_struct_boost_fusion.cpp
-./a.out
+if [[ "$NO_CXX14" == "" ]]; then
+
+$CXX $@ -std=c++14 -I. -Wall -Werror -Wextra -pedantic test_visit_struct.cpp;
+./a.out;
+
+$CXX $@ -std=c++14 -I. -I/usr/include -Wall -Werror -Wextra -pedantic test_visit_struct_boost_fusion.cpp;
+./a.out;
+
+fi
