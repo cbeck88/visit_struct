c0dc72cd4b9766db0bddc2bc39ff6028522f7ed9
Chris Beck <beck.ct@gmail.com>
2016-06-23 00:47:38 -0400

    fixup travis / test script

--- run_test.sh
+++ run_test.sh
@@ -2,18 +2,22 @@
 
 set -e
 
-$CXX $@ -std=c++11 -I. -Wall -Werror -Wextra -pedantic test_visit_struct.cpp
+if [[ "$BOOST_INCLUDE_DIR" == "" ]]; then
+  BOOST_INCLUDE_DIR="/usr/include";
+fi
+
+${CXX} $@ -std=c++11 -I. -Wall -Werror -Wextra -pedantic test_visit_struct.cpp
 ./a.out
 
-$CXX $@ -std=c++11 -I. -I/usr/include -Wall -Werror -Wextra -pedantic test_visit_struct_boost_fusion.cpp
+${CXX} $@ -std=c++11 -I. -I${BOOST_INCLUDE_DIR} -Wall -Werror -Wextra -pedantic test_visit_struct_boost_fusion.cpp
 ./a.out
 
 if [[ "$NO_CXX14" == "" ]]; then
 
-$CXX $@ -std=c++14 -I. -Wall -Werror -Wextra -pedantic test_visit_struct.cpp;
+${CXX} $@ -std=c++14 -I. -Wall -Werror -Wextra -pedantic test_visit_struct.cpp;
 ./a.out;
 
-$CXX $@ -std=c++14 -I. -I/usr/include -Wall -Werror -Wextra -pedantic test_visit_struct_boost_fusion.cpp;
+${CXX} $@ -std=c++14 -I. -I${BOOST_INCLUDE_DIR} -Wall -Werror -Wextra -pedantic test_visit_struct_boost_fusion.cpp;
 ./a.out;
 
 fi
